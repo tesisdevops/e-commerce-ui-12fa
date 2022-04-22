cdda32f967a5c30807d09c244a999a9b8e09cad1
Tesisdevops <tesisdevop2021@gmail.com>
2022-04-21 21:04:27 -0600

    Updates

--- src/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  stripe: {
-    checkoutUrl: 'INSERT AWS LAMBDA ENDPOINT FOR CHARGES',
-    productsUrl: 'https://lc9e57y685.execute-api.us-east-1.amazonaws.com/dev',
-    currency: 'EUR',
-  },
-};
-
-export default config;
